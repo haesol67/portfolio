874f4e66600e66350c46c3f35fa87fca941eaa92
haesol67 <137016999+haesol67@users.noreply.github.com>
2023-07-03 12:18:27 +0900

    HTML-Amend
    css,java,og 수정

--- src/type.js
+++ src/type.js
@@ -1,8 +1,11 @@
 'use strict';
 
-new TypeIt('.home__title--strong') //Web Publisher
+new TypeIt('.home__title--strong', {
+    loop: true,
+    speed: 150,    
+}) //Web Publisher
 .move(-13)
 .type('Amazing ') //Amazing Web Publisher
 .move(null, { to: 'END'})
-.delete()
+.pause(1000)
 .go();
